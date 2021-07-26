faa1638941e7b8bc42a88e36827129acd86e8af0
DeeakronL <deeakronplays@gmail.com>
2021-07-26 15:57:55 -0400

    more stuff with persisting

--- src/main/java/com/Deeakron/journey_mode/client/event/EventHandler.java
+++ src/main/java/com/Deeakron/journey_mode/client/event/EventHandler.java
@@ -1,13 +1,16 @@
 package com.Deeakron.journey_mode.client.event;
 
+import com.Deeakron.journey_mode.capabilities.EntityJourneyMode;
 import com.Deeakron.journey_mode.capabilities.JMCapabilityProvider;
 import com.Deeakron.journey_mode.journey_mode;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.entity.EntityEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.network.NetworkRegistry;
@@ -31,6 +34,20 @@ public class EventHandler {
             event.addCapability(new ResourceLocation(journey_mode.MODID), provider);
             event.addListener(provider::invalidate);
             journey_mode.LOGGER.info("attach capabilities event");
+        }
+    }
+
+    @SubscribeEvent
+    public static void  onPlayerClone(final PlayerEvent.Clone event) {
+        if (event.getEntity() instanceof  ServerPlayerEntity){
+            if (true){
+                journey_mode.LOGGER.info("ouch");
+                PlayerEntity original = event.getOriginal();
+                PlayerEntity newer = event.getPlayer();
+                EntityJourneyMode cap = original.getCapability(JMCapabilityProvider.INSTANCE, null).orElse(new EntityJourneyMode());
+                EntityJourneyMode cap2 = newer.getCapability(JMCapabilityProvider.INSTANCE, null).orElse(new EntityJourneyMode());
+                cap2.setJourneyMode(cap.getJourneyMode());
+            }
         }
     }
 
