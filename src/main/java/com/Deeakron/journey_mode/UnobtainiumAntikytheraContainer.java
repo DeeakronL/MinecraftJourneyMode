c9ebe78b43407e6a5431ece11a0b35902e8f0444
DeeakronL <deeakronplays@gmail.com>
2021-12-02 14:47:55 -0500

    antikythera crafting made possible

--- src/main/java/com/Deeakron/journey_mode/UnobtainiumAntikytheraContainer.java
+++ src/main/java/com/Deeakron/journey_mode/UnobtainiumAntikytheraContainer.java
@@ -1,5 +1,6 @@
 package com.Deeakron.journey_mode;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
@@ -13,29 +14,186 @@ import net.minecraft.item.crafting.*;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.network.play.server.SSetSlotPacket;
 import net.minecraft.util.IWorldPosCallable;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 import java.util.Optional;
 
-public class UnobtainiumAntikytheraContainer extends WorkbenchContainer {
-    private final CraftingInventory craftMatrix = new CraftingInventory(this, 3, 3);
+public class UnobtainiumAntikytheraContainer extends RecipeBookContainer<CraftingInventory> {
+    private final CraftingInventory craftMatrix = new CraftingInventory(this, 3, 3){
+        public void markDirty() {
+            UnobtainiumAntikytheraContainer.this.onCraftMatrixChanged(this);
+            super.markDirty();
+        }
+    };
     private final CraftResultInventory craftResult = new CraftResultInventory();
     private final IWorldPosCallable worldPosCallable;
     private final PlayerEntity player;
 
     public UnobtainiumAntikytheraContainer(int id, PlayerInventory playerInventory) {
         this(id, playerInventory, IWorldPosCallable.DUMMY);
+        journey_mode.LOGGER.info("this was the culprit");
     }
 
     public UnobtainiumAntikytheraContainer(int id, PlayerInventory playerInventory, IWorldPosCallable p_i50090_3_) {
-        super(id, playerInventory, p_i50090_3_);
+        super(JMContainerTypes.UNOBTAINIUM_ANTIKYTHERA.get(), id);
         this.worldPosCallable = p_i50090_3_;
         this.player = playerInventory.player;
+
+        this.addSlot(new CraftingResultSlot(playerInventory.player, this.craftMatrix, this.craftResult, 0, 124, 35));
+
+        for(int i = 0; i < 3; ++i) {
+            for(int j = 0; j < 3; ++j) {
+                this.addSlot(new Slot(this.craftMatrix, j + i * 3, 30 + j * 18, 17 + i * 18));
+            }
+        }
+
+        for(int k = 0; k < 3; ++k) {
+            for(int i1 = 0; i1 < 9; ++i1) {
+                this.addSlot(new Slot(playerInventory, i1 + k * 9 + 9, 8 + i1 * 18, 84 + k * 18));
+            }
+        }
+
+        for(int l = 0; l < 9; ++l) {
+            this.addSlot(new Slot(playerInventory, l, 8 + l * 18, 142));
+        }
     }
 
     public UnobtainiumAntikytheraContainer(int i, PlayerInventory playerInventory, PacketBuffer packetBuffer) {
         this(i, playerInventory);
+    }
+
+    public boolean canInteractWith(PlayerEntity playerIn) {
+        return isWithinUsableDistance(this.worldPosCallable, playerIn, UnobtainBlockInit.UNOBTAINIUM_ANTIKYTHERA.get());
+    }
+
+    @Override
+    public void fillStackedContents(RecipeItemHelper itemHelperIn) {
+        this.craftMatrix.fillStackedContents(itemHelperIn);
+    }
+
+    @Override
+    public void clear() {
+        this.craftMatrix.clear();
+        this.craftResult.clear();
+    }
+
+    @Override
+    public boolean matches(IRecipe<? super CraftingInventory> recipeIn) {
+        return recipeIn.matches(this.craftMatrix, this.player.world);
+    }
+
+    public void onContainerClosed(PlayerEntity playerIn) {
+        super.onContainerClosed(playerIn);
+        //this.worldPosCallable.consume((p_217068_2_, p_217068_3_) -> {
+        this.clearContainer(playerIn, playerIn.world, this.craftMatrix);
+        //});*/
+    }
+
+    @Override
+    public int getOutputSlot() {
+        return 0;
+    }
+
+    @Override
+    public int getWidth() {
+        return this.craftMatrix.getWidth();
+    }
+
+    @Override
+    public int getHeight() {
+        return this.craftMatrix.getHeight();
+    }
+
+    @Override
+    public int getSize() {
+        return 10;
+    }
+
+    @Override
+    public RecipeBookCategory func_241850_m() {
+        return RecipeBookCategory.CRAFTING;
+    }
+
+    public void onCraftMatrixChanged(IInventory inventoryIn) {
+        journey_mode.LOGGER.info("part 1");
+        //this.worldPosCallable.consume((p_217069_1_, p_217069_2_) -> {
+            journey_mode.LOGGER.info("part 2");
+            this.updateCraftingResult(this.windowId, this.player.world, this.player, this.craftMatrix, this.craftResult);
+        //});
+    }
+
+    protected void updateCraftingResult(int id, World world, PlayerEntity player, CraftingInventory inventory, CraftResultInventory inventoryResult) {
+        journey_mode.LOGGER.info("part 3");
+        if (!world.isRemote) {
+            journey_mode.LOGGER.info("part 4");
+            ServerPlayerEntity serverplayerentity = (ServerPlayerEntity)player;
+            ItemStack itemstack = ItemStack.EMPTY;
+            Optional<ICraftingRecipe> optional = world.getServer().getRecipeManager().getRecipe(IRecipeType.CRAFTING, inventory, world);
+            if (optional.isPresent()) {
+                ICraftingRecipe icraftingrecipe = optional.get();
+                if (inventoryResult.canUseRecipe(world, serverplayerentity, icraftingrecipe)) {
+                    itemstack = icraftingrecipe.getCraftingResult(inventory);
+                }
+            }
+
+            inventoryResult.setInventorySlotContents(0, itemstack);
+            serverplayerentity.connection.sendPacket(new SSetSlotPacket(id, 0, itemstack));
+        }
+    }
+
+    public ItemStack transferStackInSlot(PlayerEntity playerIn, int index) {
+        ItemStack itemstack = ItemStack.EMPTY;
+        Slot slot = this.inventorySlots.get(index);
+        if (slot != null && slot.getHasStack()) {
+            ItemStack itemstack1 = slot.getStack();
+            itemstack = itemstack1.copy();
+            if (index == 0) {
+                this.worldPosCallable.consume((p_217067_2_, p_217067_3_) -> {
+                    itemstack1.getItem().onCreated(itemstack1, p_217067_2_, playerIn);
+                });
+                if (!this.mergeItemStack(itemstack1, 10, 46, true)) {
+                    return ItemStack.EMPTY;
+                }
+
+                slot.onSlotChange(itemstack1, itemstack);
+            } else if (index >= 10 && index < 46) {
+                if (!this.mergeItemStack(itemstack1, 1, 10, false)) {
+                    if (index < 37) {
+                        if (!this.mergeItemStack(itemstack1, 37, 46, false)) {
+                            return ItemStack.EMPTY;
+                        }
+                    } else if (!this.mergeItemStack(itemstack1, 10, 37, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                }
+            } else if (!this.mergeItemStack(itemstack1, 10, 46, false)) {
+                return ItemStack.EMPTY;
+            }
+
+            if (itemstack1.isEmpty()) {
+                slot.putStack(ItemStack.EMPTY);
+            } else {
+                slot.onSlotChanged();
+            }
+
+            if (itemstack1.getCount() == itemstack.getCount()) {
+                return ItemStack.EMPTY;
+            }
+
+            ItemStack itemstack2 = slot.onTake(playerIn, itemstack1);
+            if (index == 0) {
+                playerIn.dropItem(itemstack2, false);
+            }
+        }
+        this.onCraftMatrixChanged(this.craftMatrix);
+
+        return itemstack;
+    }
+
+    public boolean canMergeSlot(ItemStack stack, Slot slotIn) {
+        return slotIn.inventory != this.craftResult && super.canMergeSlot(stack, slotIn);
     }
 }
