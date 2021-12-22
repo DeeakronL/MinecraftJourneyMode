7a6a89a0c30c1ca87d4d91599b3028c892f81549
Deeakron <Deeakron@gmail.com>
2021-12-22 15:33:41 -0500

    more starforge progress

--- src/main/java/com/Deeakron/journey_mode/StarforgeContainer.java
+++ src/main/java/com/Deeakron/journey_mode/StarforgeContainer.java
@@ -1,5 +1,6 @@
 package com.Deeakron.journey_mode;
 
+import com.Deeakron.journey_mode.util.FunctionalIntReferenceHolder;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.container.Container;
@@ -8,6 +9,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IWorldPosCallable;
+import net.minecraftforge.items.SlotItemHandler;
 
 import javax.annotation.Nonnull;
 import java.util.Objects;
@@ -16,9 +18,35 @@ public class StarforgeContainer extends Container {
 
     private UnobtainiumStarforgeTileEntity tileEntity;
     private IWorldPosCallable canInteractWithCallable;
+    public FunctionalIntReferenceHolder currentSmeltTime;
 
     public StarforgeContainer(final int windowID, final PlayerInventory playerInv, final UnobtainiumStarforgeTileEntity tile) {
         super(JMContainerTypes.UNOBTAINIUM_STARFORGE.get(), windowID);
+        this.canInteractWithCallable = IWorldPosCallable.of(tile.getWorld(), tile.getPos());
+
+        final int slotSizePlus2 = 18;
+        final int startX = 8;
+
+        int hotbarY = 142;
+        for (int column = 0; column < 9; column++) {
+            this.addSlot(new Slot(playerInv, column, startX + (column * slotSizePlus2), hotbarY));
+        }
+
+        final int startY = 84;
+        for (int row = 0; row < 3; row++) {
+            for (int column = 0; column < 9; column++) {
+                this.addSlot(new Slot(playerInv, 9 + (row * 9) + column, startX + (column * slotSizePlus2), startY + (row * slotSizePlus2)));
+            }
+        }
+
+        // Starforge Input
+        this.addSlot(new SlotItemHandler(tile.getInventory(), 0, 56, 17));
+        // Starforge Output
+        this.addSlot(new SlotItemHandler(tile.getInventory(), 1, 116, 35));
+        // Starforge Fuel
+        this.addSlot(new SlotItemHandler(tile.getInventory(),2, 56, 53));
+
+        this.trackInt(currentSmeltTime = new FunctionalIntReferenceHolder(() -> this.tileEntity.currentSmeltTime, value -> this.tileEntity.currentSmeltTime = value));
     }
 
     public StarforgeContainer(final int windowID, final PlayerInventory playerInv, final PacketBuffer data) {
