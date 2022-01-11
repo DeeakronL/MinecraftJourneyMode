7888b16e78d7e5038b77ed40a6c13aff1a69aa0b
DeeakronL <deeakronplays@gmail.com>
2022-01-11 17:44:20 -0500

    subtitle fix and added functionality for combat research

--- src/main/java/com/Deeakron/journey_mode/JMSounds.java
+++ src/main/java/com/Deeakron/journey_mode/JMSounds.java
@@ -12,4 +12,6 @@ public class JMSounds {
     public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, journey_mode.MODID);
 
     public static final RegistryObject<SoundEvent> RESEARCH_GRIND = SOUNDS.register("block.journey_mode.research_grinder.grind", () -> new SoundEvent(new ResourceLocation(journey_mode.MODID, "block.journey_mode.research_grinder.grind")));
+
+    public static final RegistryObject<SoundEvent> ANTIKYTHERA_CRAFT = SOUNDS.register("block.journey_mode.unobtainium_antikythera.craft", () -> new SoundEvent(new ResourceLocation(journey_mode.MODID, "block.journey_mode.unobtainium_antikythera.craft")));
 }
