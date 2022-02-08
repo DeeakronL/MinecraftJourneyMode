853ab4f17a73cc15f73edbaa5fe75bd2bf7e3e0a
DeeakronL <deeakronplays@gmail.com>
2022-02-08 16:23:48 -0500

    added particles and refined aetherial dust

--- src/main/java/com/Deeakron/journey_mode/item/AetherialVoidDustItem.java
+++ src/main/java/com/Deeakron/journey_mode/item/AetherialVoidDustItem.java
@@ -11,6 +11,7 @@ import net.minecraft.block.DirectionalBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemUseContext;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.properties.BlockStateProperties;
@@ -23,11 +24,14 @@ import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.jigsaw.JigsawOrientation;
 
 public class AetherialVoidDustItem extends Item {
+    private boolean resultFlag = false;
+
     public AetherialVoidDustItem(Properties properties) {
         super(properties);
     }
 
     public ActionResultType onItemUse(ItemUseContext context) {
+        resultFlag = false;
         World world = context.getWorld();
         BlockPos pos = context.getPos();
         PlayerEntity player = context.getPlayer();
@@ -35,31 +39,47 @@ public class AetherialVoidDustItem extends Item {
             Direction facing = world.getBlockState(pos).get(DirectionalBlock.FACING);
             Boolean conditional = world.getBlockState(pos).get(BlockStateProperties.CONDITIONAL);
             world.setBlockState(pos, UnobtainBlockInit.INERT_COMMAND_BLOCK.get().getDefaultState().with(InertCommandBlock.FACING, facing).with(InertCommandBlock.CONDITIONAL, conditional));
-            if(!world.isRemote){player.playSound(JMSounds.AETHERIAL_DEACTIVATE.get(), SoundCategory.BLOCKS, 1.0F, 1.0F);}
-            return ActionResultType.func_233537_a_(world.isRemote);
+            resultFlag = true;
         } else if (world.getBlockState(pos).getBlock().getDefaultState().matchesBlock(Blocks.CHAIN_COMMAND_BLOCK)) {
             Direction facing = world.getBlockState(pos).get(DirectionalBlock.FACING);
             Boolean conditional = world.getBlockState(pos).get(BlockStateProperties.CONDITIONAL);
             world.setBlockState(pos, UnobtainBlockInit.INERT_CHAIN_COMMAND_BLOCK.get().getDefaultState().with(InertCommandBlock.FACING, facing).with(InertCommandBlock.CONDITIONAL, conditional));
-            if(!world.isRemote){player.playSound(JMSounds.AETHERIAL_DEACTIVATE.get(), SoundCategory.BLOCKS, 1.0F, 1.0F);}
-            return ActionResultType.func_233537_a_(world.isRemote);
+            resultFlag = true;
         } else if (world.getBlockState(pos).getBlock().getDefaultState().matchesBlock(Blocks.REPEATING_COMMAND_BLOCK)) {
             Direction facing = world.getBlockState(pos).get(DirectionalBlock.FACING);
             Boolean conditional = world.getBlockState(pos).get(BlockStateProperties.CONDITIONAL);
             world.setBlockState(pos, UnobtainBlockInit.INERT_REPEATING_COMMAND_BLOCK.get().getDefaultState().with(InertCommandBlock.FACING, facing).with(InertCommandBlock.CONDITIONAL, conditional));
-            if(!world.isRemote){player.playSound(JMSounds.AETHERIAL_DEACTIVATE.get(), SoundCategory.BLOCKS, 1.0F, 1.0F);}
-            return ActionResultType.func_233537_a_(world.isRemote);
+            resultFlag = true;
         } else if (world.getBlockState(pos).getBlock().getDefaultState().matchesBlock(Blocks.JIGSAW)) {
             JigsawOrientation orientation = world.getBlockState(pos).get(BlockStateProperties.ORIENTATION);
             world.setBlockState(pos, UnobtainBlockInit.INERT_JIGSAW_BLOCK.get().getDefaultState().with(InertJigsawBlock.ORIENTATION, orientation));
-            if(!world.isRemote){player.playSound(JMSounds.AETHERIAL_DEACTIVATE.get(), SoundCategory.BLOCKS, 1.0F, 1.0F);}
-            return ActionResultType.func_233537_a_(world.isRemote);
+            resultFlag = true;
         } else if (world.getBlockState(pos).getBlock().getDefaultState().matchesBlock(Blocks.STRUCTURE_BLOCK)) {
             StructureMode mode = world.getBlockState(pos).get(BlockStateProperties.STRUCTURE_BLOCK_MODE);
             world.setBlockState(pos, UnobtainBlockInit.INERT_STRUCTURE_BLOCK.get().getDefaultState().with(InertStructureBlock.MODE, mode));
-            if(!world.isRemote){player.playSound(JMSounds.AETHERIAL_DEACTIVATE.get(), SoundCategory.BLOCKS, 1.0F, 1.0F);}
-            return ActionResultType.func_233537_a_(world.isRemote);
+            resultFlag = true;
         }
-        return ActionResultType.PASS;
+
+        if (resultFlag) {
+            if(!world.isRemote){player.playSound(JMSounds.AETHERIAL_DEACTIVATE.get(), SoundCategory.BLOCKS, 1.0F, 1.0F);}
+            for (int i = 0; i < 50; i++) {
+                double d0 = 3.0D;
+                double d1 = 1.0D;
+                double d2 = random.nextGaussian() * 0.02D;
+                double d3 = random.nextGaussian() * 0.02D;
+                double d4 = random.nextGaussian() * 0.02D;
+                double d5 = 0.5D - d0;
+                double d6 = (double)pos.getX()  + random.nextDouble();// * d0 - 0.5D;
+                double d7 = (double)pos.getY() + random.nextDouble();// * d1;
+                double d8 = (double)pos.getZ() + random.nextDouble();// * d0 - 0.5D;
+                world.addParticle(ParticleTypes.SMOKE, d6, d7, d8, d2, d3, d4);
+            }
+
+            return ActionResultType.func_233537_a_(world.isRemote);
+        } else {
+            return ActionResultType.PASS;
+        }
+
+
     }
 }
