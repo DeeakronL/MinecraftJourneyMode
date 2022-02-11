186fe3d5ec94e733cf4d7bfb6e18ac051dcf6ecf
DeeakronL <deeakronplays@gmail.com>
2022-02-11 15:53:43 -0500

    changing item check

--- src/main/java/com/Deeakron/journey_mode/block/PaintedBarrierBlock.java
+++ src/main/java/com/Deeakron/journey_mode/block/PaintedBarrierBlock.java
@@ -1,14 +1,28 @@
 package com.Deeakron.journey_mode.block;
 
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockRenderType;
-import net.minecraft.block.BlockState;
+import com.Deeakron.journey_mode.init.UnobtainItemInit;
+import com.Deeakron.journey_mode.journey_mode;
+import net.minecraft.block.*;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.Item;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.state.EnumProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.state.properties.StructureMode;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 
 public class PaintedBarrierBlock extends Block {
     public PaintedBarrierBlock(AbstractBlock.Properties properties) {
@@ -19,4 +33,25 @@ public class PaintedBarrierBlock extends Block {
     }
 
 
+
+    @OnlyIn(Dist.CLIENT)
+    public void animateTick(BlockState state, World worldIn, BlockPos pos, Random random) {
+        List<? extends PlayerEntity> players = worldIn.getPlayers();
+        for (PlayerEntity player :players) {
+            if (player.getItemStackFromSlot(EquipmentSlotType.MAINHAND).getItem() instanceof BlockItem) {
+                if (((BlockItem) player.getItemStackFromSlot(EquipmentSlotType.MAINHAND).getItem()).getBlock() == Blocks.BARRIER){
+                    int i = pos.getX();
+                    int j = pos.getY();
+                    int k = pos.getZ();
+                    worldIn.addParticle(ParticleTypes.BARRIER, (double)i + 0.5D, (double)j + 0.5D, (double)k + 0.5D, 0.0D, 0.0D, 0.0D);
+                }
+            } else if (player.getItemStackFromSlot(EquipmentSlotType.MAINHAND).getItem() == UnobtainItemInit.PAINT_BUCKET.get()){
+                int i = pos.getX();
+                int j = pos.getY();
+                int k = pos.getZ();
+                worldIn.addParticle(ParticleTypes.BARRIER, (double)i + 0.5D, (double)j + 0.5D, (double)k + 0.5D, 0.0D, 0.0D, 0.0D);
+            }
+        }
+        super.animateTick(state, worldIn, pos, random);
+    }
 }
